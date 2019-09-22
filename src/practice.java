b7901a704dd427e66bb24cfb7f73d289d9d28c3d
mosheavidor1 <mosheavidorster@gmail.com>
2019-09-22 12:16:45 +0300

    Initial commit

---
+++ src/practice.java
@@ -0,0 +1,20 @@
+public class practice {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("hello world");
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
